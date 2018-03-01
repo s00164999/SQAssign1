891d47007cd01bf14c804efd10b29c9c1bf38724
Artur Harla <s00164999@mail.itsligo.ie>
2018-03-01 23:22:04 +0000

    Test

---
+++ SQAssign1.tests/TestClass1.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQAssign1.tests
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        static float expectedResult;
+        static Class1 c1;
+
+        [SetUp]
+        public static void Init()
+        {
+            c1 = new Class1();
+        }
+
+        [Test(Description = "Test object is not null")]
+        public void Test1()
+        {
+            Assert.NotNull(c1, "Object not created");
+        }
+
+        [Test(Description = "Test premium")]
+        public static void Test2()
+        {
+            expectedResult = 0; //Dummy data, to be replaced
+            float actualResult = c1.CalcPremium(1, ""); //Dummy data, to be replaced
+        }
+        [TestCase(0, "", ExpectedResult = 0)] //Dummy data, to be replaced
+        [TestCase(0, "", ExpectedResult = 0)] //Dummy data, to be replaced
+        [TestCase(0, "", ExpectedResult = 0)] //Dummy data, to be replaced
+        [TestCase(0, "", ExpectedResult = 0)] //Dummy data, to be replaced
+        [TestCase(0, "", ExpectedResult = 0)] //Dummy data, to be replaced
+        [TestCase(0, "", ExpectedResult = 0)] //Dummy data, to be replaced
+
+        [Test(Description = "Test out of range values")]
+        public static void Test3()
+        {
+            expectedResult = 0; //Dummy data, to be replaced
+            float actualResult = c1.CalcPremium(1, "");
+        }
+    }
+}
--- SQAssign1/Class1.cs
+++ SQAssign1/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using fit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,78 @@ using System.Threading.Tasks;
 
 namespace SQAssign1
 {
+    public class Class1Test : ColumnFixture
+    {
+        public int age;
+        public string gen;
+
+        public Class1 c1 = new Class1();
+
+        public float Check()
+        {
+            return c1.CalcPremium(age, gen);
+        }
+    }
+
     public class Class1
     {
+
+        public float CalcPremium(int age, string gender)
+        {
+            float premium = 0;
+
+            if (gender == "female")
+            {
+                if ((age >= 18) && (age <= 30))
+                    premium = (float)5.0;
+                else
+                {
+                    if (age >= 31)
+                        premium = (float)2.50;
+                    else
+                        premium = (float)0.0;
+                }
+            }
+            else if (gender == "male")
+            {
+                if ((age >= 18) && (age <= 35))
+                    premium = (float)6.0;
+                else
+                {
+                    if (age >= 36)
+                        premium = (float)5.0;
+                    else
+                        premium = (float)0.0;
+                }
+            }
+            else
+                premium = (float)0.0;
+
+            //if (gender == "female")
+            //{
+            //    if ((age >= 18) && (age <= 30))
+            //        premium = (float)5.0;
+            //    else if (age >= 31)
+            //        premium = (float)2.50;
+            //    else
+            //        premium = (float)0.0;
+            //}
+            //else if (gender == "male")
+            //{
+            //    if ((age >= 18) && (age <= 35))
+            //        premium = (float)6.0;
+            //    else if (age >= 36)
+            //        premium = (float)5.0;
+            //    else
+            //        premium = (float)0.0;
+            //}
+            //else
+            //    premium = (float)0.0;
+
+            if (age >= 50)
+                premium = premium * (float)0.15;
+
+            return premium;
+        }
     }
 }
